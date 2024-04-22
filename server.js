20941f596831cebc7d3c084994195fba613f2d5d
Ujjwal Kumar <ujjwal.rob11@gmail.com>
2024-04-22 10:50:23 +0530

    changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const next = require('next');
+const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
@@ -13,12 +14,16 @@ app.prepare().then(() => {
   // Serve Swagger UI at /api-docs
   server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-  // Serve Next.js app at /
-  server.get('*', (req, res) => {
+  // Serve Next.js app at all other routes
+  server.all('*', (req, res) => {
     return handle(req, res);
   });
 
-  // Deploy to Vercel domain
+  // Ensure that the swagger.json file is served correctly
+  server.get('/swagger.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'swagger.json'));
+  });
+
   const PORT = process.env.PORT || 3000;
   server.listen(PORT, (err) => {
     if (err) throw err;
