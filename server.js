26e8c680294b38349aefcc88b266b9f375738c4d
Ujjwal Kumar <ujjwal.rob11@gmail.com>
2024-04-22 16:26:34 +0530

    changes10023

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const next = require('next');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json'); // Path to your Swagger JSON file
 
-const server = express();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-// Serve Swagger UI at /api/docs
-server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.prepare().then(() => {
+  const server = express();
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`Server ready on port ${PORT}`);
+  // Body parser middleware
+  server.use(express.json());
+
+  // Serve Swagger UI at /api/docs
+  server.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  // Default route for Next.js pages
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  // Start the server
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server ready on port ${PORT}`);
+  });
 });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 // src/pages/index.js
 import React from 'react';
 import Login from '../components/auth/Login';
+import Swagger from '../pages/api/swagger'
 
 const HomePage = () => {
   return (
     <div>
       <Login />
+      <Swagger />
     </div>
   );
 };
